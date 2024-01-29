a5d412b83efe8a19ffee30bef31b6591aeb13166
Aleks Pimen <pimenaleks04@gmail.com>
2024-01-28 20:10:25 -0500

    home final

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,28 +1,37 @@
 import { Link } from 'react-router-dom';
-import '../src/index.css'
+import programming from "../src/images/programming.jpg";
+import '../src/index.css';
 
 export default function Home() {
-     return <>
-     
-     <section id="intro">
-          <div className="introContent">
-          <span className="hello">Hello,</span><br/>
-          <span className="introText">I am <span className="introName">Aleksandr Pimenov</span> 
-          <br/>Website Designer</span>
-          <Link to="/about"><button className="WebButtons">About Me</button></Link>
-          <br />
-          <br />
-          <br />
-          <Link to="/contact"><button className="WebButtons">Contact Me</button></Link>
-          <p className="introPara">My expertise lies in designing web pages that are both attractive and easy to use. I have a background in web development and graphic design, and I can create websites that suit different needs and preferences.</p>
- 
-          
-          </div>
-          
-          
-     </section>
-    
+  return (
+    <section id="intro" className="introContainer">
+      <div className="introContentLeft">
+        <span className="hello">Hello,</span>
+        <br />
+        <span className="introText">
+          I am <span className="introName">Aleksandr Pimenov</span>
+          <br />Website Designer
+          <p className="introPara">
+        My expertise lies in designing web pages that are both attractive and easy to use. I have a background in web development and graphic design, and I can create websites that suit different needs and preferences.
+      </p>
+        </span>
 
-     </>
-     }
-     
+        <div className="buttonsContainer">
+          <Link to="/about">
+            <button className="WebButtons">About Me</button>
+          </Link>
+          <Link to="/contact">
+            <button className="WebButtons">Contact Me</button>
+          </Link>
+        </div>
+      </div>
+
+      <div className="introContentRight">
+      < img src={programming}alt="programming" className="programmingImage"/>
+        
+      </div>
+
+      
+    </section>
+  );
+}
--- src/about.jsx
+++ src/about.jsx
@@ -1,8 +1,34 @@
-export default function About() {
+export default function Home() {
      return (
-          <>
-            <p>About Me</p>
-          </>
+       <section id="intro" className="introContainer">
+         <div className="introContentLeft">
+           <span className="hello">Hello,</span>
+           <br />
+           <span className="introText">
+             I am <span className="introName">Aleksandr Pimenov</span>
+             <br />Website Designer
+             <p className="introPara">
+           My expertise lies in designing web pages that are both attractive and easy to use. I have a background in web development and graphic design, and I can create websites that suit different needs and preferences.
+         </p>
+           </span>
+   
+           <div className="buttonsContainer">
+             <Link to="/about">
+               <button className="WebButtons">About Me</button>
+             </Link>
+             <Link to="/contact">
+               <button className="WebButtons">Contact Me</button>
+             </Link>
+           </div>
+         </div>
+   
+         <div className="introContentRight">
+         < img src={programming}alt="programming" className="programmingImage"/>
+           
+         </div>
+   
+         
+       </section>
      );
-}
+   }
      
