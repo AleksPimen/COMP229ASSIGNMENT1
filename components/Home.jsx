ee60b17b768f433d9fd5969ddf955b70dc1da6b7
Aleks Pimen <pimenaleks04@gmail.com>
2024-01-28 19:40:36 -0500

    home page edits

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,4 +1,28 @@
+import { Link } from 'react-router-dom';
+import '../src/index.css'
+
 export default function Home() {
-     return <p>Hello World!</p>
-}
+     return <>
+     
+     <section id="intro">
+          <div className="introContent">
+          <span className="hello">Hello,</span><br/>
+          <span className="introText">I am <span className="introName">Aleksandr Pimenov</span> 
+          <br/>Website Designer</span>
+          <Link to="/about"><button className="WebButtons">About Me</button></Link>
+          <br />
+          <br />
+          <br />
+          <Link to="/contact"><button className="WebButtons">Contact Me</button></Link>
+          <p className="introPara">My expertise lies in designing web pages that are both attractive and easy to use. I have a background in web development and graphic design, and I can create websites that suit different needs and preferences.</p>
+ 
+          
+          </div>
+          
+          
+     </section>
+    
+
+     </>
+     }
      
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../src/images/logo.ico';
+
 export default function Layout() {
     return (
     <>
-    <h1>My Portfolio</h1>
+    
+    <h1>
+        < img src={logo}alt="logo" className="logo" width="50px" height="50px"/>My Portfolio
+    </h1>
     <nav>
-     <Link to="/">Home</Link> | <Link to="/about">About Me</Link> | <Link to="/education">Services</Link>| <Link to="/project">Projects Page</Link>| <Link to="/contact">Contact Me</Link>
+     <Link to="/">Home</Link> | <Link to="/about">About Me</Link> | <Link to="/services">Services</Link>| <Link to="/project">Projects Page</Link>| <Link to="/contact">Contact Me</Link>
     </nav>
     <hr />
 
