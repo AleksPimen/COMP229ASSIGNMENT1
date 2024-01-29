f1d1d5e6557b428f5aac0574782d0b1c7ddd158b
Aleks Pimen <pimenaleks04@gmail.com>
2024-01-28 23:49:26 -0500

    finnal

--- src/project.jsx
+++ src/project.jsx
@@ -1,30 +1,43 @@
-import '../src/project.css'
+import bug from '../src/images/buggame.png';
+import slide from '../src/images/slideshow.png';
+import mult from '../src/images/multiplication.png';
+import '../src/index.css';
+import '../src/project.css';
+
 export default function Project() {
-     return (
-     <>
-     <br /> <br /> <br />
-     <p>Contact Me</p>
-     <form id="ffp">
-		
-			
-				<label htmlFor="myFName"> *First Name: </label>
-				<input type="text" id="myFName" name="myFName" required="required" autoFocus /> <br /> <br />
-				
-				<label htmlFor="myLName"> *Last Name: </label>
-				<input type="text" id="myLName" name="myLName" required="required" /> <br /> <br />
-				
-				
-				<label htmlFor="myEmail"> *Email: </label>
-				<input type="email" id="myEmail" name="myEmail" required="required" /> <br /> <br />
-				
-				<input type="submit" className="submit" value="Submit" /> <br />
-					
-		
-		</form> <br></br>
-          <p><i>Call me on: <strong>437.991.5855 </strong></i></p>
- <p>Aleksandr Pimenov</p>
- <p>apimeno1@my.centennialcollege.ca</p>
-     </>
-     );
-     }
-    
+  return (
+      <>
+      <br /><br /><br /><br />
+
+      <section id="skill">
+      <span className="skillTitle">Projects</span>
+          <div className="skillBar">
+          <img src={bug} alt="backend" className="skillBarImg" />
+               <div className="skillBarText">
+               <h2>Bug Smasher</h2>
+               <p>This is a bug smashing game that I developed using HTML, CSS and JavaScript. The game involves clicking on bugs that appear randomly on the screen. I was responsible for both the frontend and backend development of the game, which involved designing the user interface, creating the game logic, and implementing the scoring system.</p>
+               </div>
+
+          </div>
+          
+          <div className="skillBar">
+          <img src={slide} alt="frontend" className="skillBarImg" />
+               <div className="skillBarText">
+               <h2>Interactive Slideshow</h2>
+               <p>I created an interactive slideshow website that showcases multiple images using HTML, CSS and JavaScript. I was responsible for both the frontend and backend development of this website.</p>
+               </div>
+
+          </div>
+
+          <div className="skillBar">
+          <img src={mult} alt="AppDesign" className="skillBarImg" />
+               <div className="skillBarText">
+               <h2>Multiplication Table</h2>
+               <p>This is a Multiplication Table website, a project that I created using HTML, CSS and JavaScript. Website which allows users to generate multiplication tables of any size they want. You can enter the number of rows and columns you want, and the website will display the table for you. </p>
+               </div>
+
+          </div>
+          </section>
+      </>
+  );
+}
